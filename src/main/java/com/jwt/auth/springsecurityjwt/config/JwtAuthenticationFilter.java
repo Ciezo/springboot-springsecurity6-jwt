b0b9f3917794aee472e377b3a91b8358e23f48e1
Cloyd Van <abcd123.letters@gmail.com>
2024-02-14 14:26:36 +0800

    Todo

---
+++ src/main/java/com/jwt/auth/springsecurityjwt/config/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+/**
+ * @author Cloyd Van Secuya
+ *
+ * <p>
+ *     The JWT Authentication Filter which is active for every request
+ *     sent by the user.
+ * </p>
+ *
+ * <p>
+ *     <code>@Service</code> is used here because here I want to separate it
+ *     from the rest of the application, and make it usable onto other parts
+ *     such as the presentation layer (Controller) or persistence layer (Repository).
+ * </p>
+ */
+package com.jwt.auth.springsecurityjwt.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    /**
+     * @note The parameters should not be empty!
+     * @param request User request for authentication
+     * @param response Response body returned by the Server
+     * @param filterChain JWT Auth Filter, a design by Spring Security
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        /**
+         * @// TODO: 2/14/2024
+         * Implement a check jwt auth filter
+         */
+    }
+}
