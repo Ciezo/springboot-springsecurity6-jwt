207b194a333115c29f9ec11b7632e430794a3498
Cloyd Van <abcd123.letters@gmail.com>
2024-02-14 15:51:54 +0800

    Documentation about Claims in JWTs

--- src/main/java/com/jwt/auth/springsecurityjwt/service/JwtService.java
+++ src/main/java/com/jwt/auth/springsecurityjwt/service/JwtService.java
@@ -4,6 +4,34 @@
  * <p>
  *     This class is used to extract the User credentials based on JWT (token)
  * </p>
+ *
+ * <h5>Claims</h5>
+ * <p>
+ *     <b>Claims</b> are part of the JSON Web Tokens (JWTs).
+ *     These are bits of information about a certain subject, say, "name" of a person.
+ *     Furthermore, Claims help us gather information about a User or an object
+ * </p>
+ * <p>
+ *     <u>More examples of Claims:</u>
+ *     <ol>
+ *         <li>sub</li>
+ *         <li>iat</li>
+ *         <li>role</li>
+ *     </ol>
+ * </p>
+ *
+ * <h5>Registered Claims:</h5>
+ * <p>
+ *     <ol>
+ *         <li>iss (issuer)</li>
+ *         <li>sub (subject)</li>
+ *         <li>aud (audience)</li>
+ *         <li>exp (expiration time)</li>
+ *         <li>nbf (not before time)</li>
+ *         <li>iat (issued at time)</li>
+ *         <li>jti (JWT ID)</li>
+ *     </ol>
+ * </p>
  */
 package com.jwt.auth.springsecurityjwt.service;
 
