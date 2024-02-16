d2925d354a9f9fa49aab195a1c2d1d153cffb643
Cloyd Van <abcd123.letters@gmail.com>
2024-02-16 15:11:07 +0800

    AuthenicationProvider
    Preparing to create RESTful endpoints to register user and authenticate existing user

---
+++ src/main/java/com/jwt/auth/springsecurityjwt/auth/AuthenticationController.java
@@ -0,0 +1,27 @@
+/**
+ * @author Cloyd Van Secuya
+ *
+ * <p>
+ *     An endpoint to register an account, or authenticate an existing User.
+ * </p>
+ *
+ */
+package com.jwt.auth.springsecurityjwt.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+/**
+ * @// TODO: 2/16/2024
+ * - create endpoints to register a new user
+ * - create endpoints to authenticate an existing user
+ * ☺️
+ */
+
+}
--- src/main/java/com/jwt/auth/springsecurityjwt/config/SecurityConfig.java
+++ src/main/java/com/jwt/auth/springsecurityjwt/config/SecurityConfig.java
@@ -8,17 +8,42 @@
 package com.jwt.auth.springsecurityjwt.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    /**
-     * @// TODO: 2/15/2024
-     * - add configuring all the HTTP security for requests from the Client
-     */
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests()
+                /* Whitelisted URLs */
+                .requestMatchers("")
+
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
 }
