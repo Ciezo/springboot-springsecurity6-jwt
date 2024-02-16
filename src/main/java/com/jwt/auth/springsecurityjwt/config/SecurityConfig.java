d595bc0edc27181899e1ba360b69fd9c8216a1f7
Cloyd Van <abcd123.letters@gmail.com>
2024-02-16 17:17:29 +0800

    Whitelist everything under the /api/v1/auth

--- src/main/java/com/jwt/auth/springsecurityjwt/config/SecurityConfig.java
+++ src/main/java/com/jwt/auth/springsecurityjwt/config/SecurityConfig.java
@@ -32,8 +32,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests()
                 /* Whitelisted URLs */
-                .requestMatchers("")
-
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
