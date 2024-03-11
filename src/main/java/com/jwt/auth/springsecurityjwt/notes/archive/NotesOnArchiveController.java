377c6e6cd23bb85f49e1837df056efa56476cceb
Cloyd Van <abcd123.letters@gmail.com>
2024-03-11 16:52:20 +0800

    Controllers for moving notes to archive and trash resources
    TODO is listed

---
+++ src/main/java/com/jwt/auth/springsecurityjwt/notes/archive/NotesOnArchiveController.java
@@ -0,0 +1,15 @@
+package com.jwt.auth.springsecurityjwt.notes.archive;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @// TODO: 3/11/2024
+ * - Create a controller endpoint to assign resources for archived notes
+ * - Use the NotesService to integrate common notes functions
+ */
+@RestController
+@RequestMapping("/api/inkdown/v1/notes-archive")
+public class NotesOnArchiveController {
+
+}
---
+++ src/main/java/com/jwt/auth/springsecurityjwt/notes/trash/NotesOnTrashController.java
@@ -0,0 +1,14 @@
+package com.jwt.auth.springsecurityjwt.notes.trash;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @// TODO: 3/11/2024
+ * - Create a controller endpoint to assign all resources related to trashed notes
+ * - Use the NotesService to integrate common notes functions
+ */
+@RestController
+@RequestMapping("/api/inkdown/v1/notes-trash")
+public class NotesOnTrashController {
+}
