7e778b6d00166b6593619b0fe58a56073607d546
Cloyd Van <abcd123.letters@gmail.com>
2024-03-12 14:43:31 +0800

    Processing single requests

--- src/main/java/com/jwt/auth/springsecurityjwt/auth/AuthenticationController.java
+++ src/main/java/com/jwt/auth/springsecurityjwt/auth/AuthenticationController.java
@@ -8,6 +8,7 @@
  */
 package com.jwt.auth.springsecurityjwt.auth;
 
+import com.jwt.auth.springsecurityjwt.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @RestController
@@ -33,21 +36,43 @@ public class AuthenticationController {
     public ResponseEntity<?> register (
             @RequestBody Object request
     ) {
+        try {
+            /**
+             * For Debugging requests
+             * //////////////////////////////////////////////////////////////////////// */
+            /* Uncomment both prints to debug the request values and types */
+            // System.out.println("Class Type: " + request.getClass().getName());
+            // System.out.println("Request: " + request.toString());
+            /** * //////////////////////////////////////////////////////////////////// */
 
-        if (request instanceof RegisterRequest) {
-            RegisterRequest singleRequest = (RegisterRequest) request;
-            return ResponseEntity.ok(
-                    authService.register(singleRequest)
-            );
-        } else if (request instanceof List) {
-            List<RegisterRequest> batchRequests = (List<RegisterRequest>) request;
-            return ResponseEntity.ok(
-                    authService.registerBatch(batchRequests)
-            );
-        } else {
-            return ResponseEntity
-                    .badRequest()
-                    .body("Invalid request format");
+            if (request instanceof LinkedHashMap<?,?>) {
+                RegisterRequest singleRequest =
+                        new RegisterRequest(
+                                (String) ((LinkedHashMap<?, ?>) request).get("firstname"),
+                                (String) ((LinkedHashMap<?, ?>) request).get("lastname"),
+                                (String) ((LinkedHashMap<?, ?>) request).get("username"),
+                                (String) ((LinkedHashMap<?, ?>) request).get("email"),
+                                (String) ((LinkedHashMap<?, ?>) request).get("password"),
+                                (Role) ((LinkedHashMap<?, ?>) request).get("role")
+                        );
+                return ResponseEntity.ok(
+                        authService.register(singleRequest)
+                );
+            } else if (request instanceof ArrayList<?>) {
+                List<RegisterRequest> batchRequests = (List<RegisterRequest>) request;
+                return ResponseEntity.ok(
+                        authService.registerBatch(batchRequests)
+                );
+            } else {
+                return ResponseEntity
+                        .badRequest()
+                        .body("Invalid request format");
+            }
+
+        } catch(Exception err) {
+            System.out.println("An error occurred in deserialization of request");
+            System.out.println("Error: " + err);
+            throw err;
         }
 
     }
