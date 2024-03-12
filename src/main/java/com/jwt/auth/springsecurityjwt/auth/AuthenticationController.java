c4e5b7c6bb42608f70379f561cf2ca6ea537455c
Cloyd Van <abcd123.letters@gmail.com>
2024-03-12 14:58:21 +0800

    Changes in implementation
    I did a very complex solution for a very simple task....
    
    Now, I realize I cannot always over-engineer...
    
    To handle multiple requests I have set the threads to handle a max of 200 requests running simultaneously.
    This is called synchronous handling, and Spring Boot using Tomcat server can do that!
    
    Reference: https://stackoverflow.com/questions/46893237/can-spring-boot-application-handle-multiple-requests-simultaneously

--- src/main/java/com/jwt/auth/springsecurityjwt/auth/AuthenticationController.java
+++ src/main/java/com/jwt/auth/springsecurityjwt/auth/AuthenticationController.java
@@ -8,17 +8,12 @@
  */
 package com.jwt.auth.springsecurityjwt.auth;
 
-import com.jwt.auth.springsecurityjwt.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -33,50 +28,17 @@ public class AuthenticationController {
      * @return store user in the database with a token
      */
     @PostMapping("/register")
-    public ResponseEntity<?> register (
-            @RequestBody Object request
+    public ResponseEntity<AuthenticationResponse> register (
+            @RequestBody RegisterRequest request
     ) {
-        try {
-            /**
-             * For Debugging requests
-             * //////////////////////////////////////////////////////////////////////// */
-            /* Uncomment both prints to debug the request values and types */
-            // System.out.println("Class Type: " + request.getClass().getName());
-            // System.out.println("Request: " + request.toString());
-            /** * //////////////////////////////////////////////////////////////////// */
-
-            if (request instanceof LinkedHashMap<?,?>) {
-                RegisterRequest singleRequest =
-                        new RegisterRequest(
-                                (String) ((LinkedHashMap<?, ?>) request).get("firstname"),
-                                (String) ((LinkedHashMap<?, ?>) request).get("lastname"),
-                                (String) ((LinkedHashMap<?, ?>) request).get("username"),
-                                (String) ((LinkedHashMap<?, ?>) request).get("email"),
-                                (String) ((LinkedHashMap<?, ?>) request).get("password"),
-                                (Role) ((LinkedHashMap<?, ?>) request).get("role")
-                        );
-                return ResponseEntity.ok(
-                        authService.register(singleRequest)
-                );
-            } else if (request instanceof ArrayList<?>) {
-                List<RegisterRequest> batchRequests = (List<RegisterRequest>) request;
-                return ResponseEntity.ok(
-                        authService.registerBatch(batchRequests)
-                );
-            } else {
-                return ResponseEntity
-                        .badRequest()
-                        .body("Invalid request format");
-            }
-
-        } catch(Exception err) {
-            System.out.println("An error occurred in deserialization of request");
-            System.out.println("Error: " + err);
-            throw err;
-        }
-
+            return ResponseEntity.ok(authService.register(request));
     }
 
+    /**
+     * Authenticate Existing User from the database
+     * @param request using username and password as credentials
+     * @return authenticated user with a token
+     */
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate (
             @RequestBody AuthenticationRequest request
