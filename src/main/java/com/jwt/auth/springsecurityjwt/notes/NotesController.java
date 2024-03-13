b3e13324a36a6a349c9987a51a04ebe49a2087f1
Cloyd Van <abcd123.letters@gmail.com>
2024-03-13 12:15:03 +0800

    Documentation comments added
    Documentation and @TODO
    Some problems occurred after testing the endpoints with Postman.

--- src/main/java/com/jwt/auth/springsecurityjwt/notes/NotesController.java
+++ src/main/java/com/jwt/auth/springsecurityjwt/notes/NotesController.java
@@ -16,6 +16,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
+/**
+ * @TODO
+ * - IMPLEMENT QUERY PARAMETERS.
+ *   My mapping and endpoints are very ambiguous. Hence, an error occurs:
+ *
+ *   ```java.lang.IllegalStateException: Ambiguous handler methods mapped for '/api/inkdown/v1/notes/104'```
+ *
+ *   A query parameter is used to identify different types of resources using a specific endpoint.
+ *   Examples:
+ *   http://localhost:18080/api/inkdown/v1/notes/104
+ *
+ *       - where this URL is using Id to GET a single Note.
+ *
+ *   http://localhost:18080/api/inkdown/v1/notes?title=Sharks
+ *
+ *       - where this URL is using GET to identify a single Note by title.
+ *
+ *  In conclusion, it is better to implement both ways, /id and /{param}, to GET or Map a resource.
+ *
+ */
 @RestController
 @RequestMapping("/api/inkdown/v1/notes")
 public class NotesController {
@@ -40,6 +61,10 @@ public class NotesController {
         );
     }
 
+    /** @// TODO: 3/13/2024 fix this.
+     * This is an ambiguous endpoint
+     * I must use a query parameter, /notes?title=""
+     * <b>Use the @RequestParam in the argument</b>*/
     @GetMapping("/{noteTitle}")
     public ResponseEntity<Optional<Notes>> getSingleNoteByTitle
             (@PathVariable("noteTitle") String noteTitle) {
@@ -49,6 +74,10 @@ public class NotesController {
         );
     }
 
+    /** @// TODO: 3/13/2024 fix this.
+     * This is an ambiguous endpoint
+     * I must use a query parameter, /notes?author=""
+     * <b>Use the @RequestParam in the argument</b>*/
     @GetMapping("/{noteAuthor}")
     public ResponseEntity<Optional<Notes>> getSingleNoteByAuthor(
             @PathVariable("noteAuthor") String noteAuthor) {
@@ -67,6 +96,9 @@ public class NotesController {
         );
     }
 
+    /** @// TODO: 3/13/2024 fix this.
+     * The endpoint assigned is okay, however, it only returns
+     * the current or existing note, but <b>not the updated note.</b> */
     @PutMapping("/update-note/id/{noteId}")
     public ResponseEntity<Notes> updateNote(
             @RequestBody Notes note,
@@ -77,6 +109,9 @@ public class NotesController {
         );
     }
 
+    /** @TODO: 3/13/2024 test this.
+     * I have not tested this yet. I hope it works when the time comes
+     * :crying_emoji: :crying_emoji: :crying_emoji: :crying_emoji: */
     @DeleteMapping("/delete-note/id/{noteId}")
     public void deleteNote(
             @PathVariable("noteId") long id) {
